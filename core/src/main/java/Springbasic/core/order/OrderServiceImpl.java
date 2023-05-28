3baae2acb603c14025c6e230d13922a8a4fe1ff3
junseok <junseok.oh@gentlepie.com>
2023-05-28 17:33:52 +0900

    현재 인터페이스 문제점

--- core/src/main/java/Springbasic/core/order/OrderServiceImpl.java
+++ core/src/main/java/Springbasic/core/order/OrderServiceImpl.java
@@ -1,7 +1,6 @@
 package Springbasic.core.order;
 
 import Springbasic.core.discount.DiscountPolicy;
-import Springbasic.core.discount.FixDiscountPolicy;
 import Springbasic.core.member.Member;
 import Springbasic.core.member.MemberRepository;
 import Springbasic.core.member.MemoryMemberRepository;
@@ -9,7 +8,13 @@ import Springbasic.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements  OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    // 추상화에만 의존하게 변경 => 인터페이스에만 의존
+    // -> 이것만 변경하면 null 이슈 발생
+    // -> 누군가 클라이언트 OrderServiceImpl 에 discountPolicy 의 구현객체를 대신 생성해줘야 한다.
+    private DiscountPolicy discountPolicy;
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
