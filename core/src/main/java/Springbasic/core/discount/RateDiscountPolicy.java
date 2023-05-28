cce0dfcabab07c1c260e8ff0d4872fb843d0b666
junseok <junseok.oh@gentlepie.com>
2023-05-28 17:23:13 +0900

    rate

---
+++ core/src/main/java/Springbasic/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,17 @@
+package Springbasic.core.discount;
+
+import Springbasic.core.member.Grade;
+import Springbasic.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPercent = 10;
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
---
+++ core/src/test/java/Springbasic/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,37 @@
+package Springbasic.core.discount;
+
+import Springbasic.core.member.Grade;
+import Springbasic.core.member.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("vip 10% 할인")
+    void vip_o() {
+        //given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        assertThat(discount).isEqualTo(1000);
+
+        //static import
+    }
+
+    @Test
+    @DisplayName("VIP 아닌 경우")
+    void vip_x() {
+        //given
+        Member member = new Member(2L, "memberBasic", Grade.BASIC);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        assertThat(discount).isEqualTo(0);
+    }
+}
